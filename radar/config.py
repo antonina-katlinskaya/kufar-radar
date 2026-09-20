87bfe912cb719372726b139228e8315d300eb47c
antonina-katlinskaya <294815314+antonina-katlinskaya@users.noreply.github.com>
2026-09-20 13:28:13 +0300

    config: target Alena within Etazhi profile

--- radar/config.py
+++ radar/config.py
@@ -5,6 +5,7 @@ import os
 class Settings:
     kufar_profile_id: str = os.getenv('KUFAR_PROFILE_ID', '11093294')
     kufar_profile_url: str = os.getenv('KUFAR_PROFILE_URL', 'https://re.kufar.by/p/ooo-mezhdunarodnaya-rielterskaya-kompaniya-etazhi/11093294')
+    kufar_contact_person: str = os.getenv('KUFAR_CONTACT_PERSON', 'Алёна')
     bir_search_url: str = os.getenv('BIR_SEARCH_URL', 'https://bir.by/search-by-parameters/')
     bir_refresh_minutes: int = int(os.getenv('BIR_REFRESH_MINUTES', '15'))
     telegram_token: str = os.getenv('TELEGRAM_BOT_TOKEN', '')
